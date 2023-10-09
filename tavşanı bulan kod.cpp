0775d4666d207818286978482e29bf0401b80d05
Yalçın Fırat Uluğ <121889136+wildvillain@users.noreply.github.com>
2023-10-09 20:36:41 +0300

    Update tavşanı bulan kod.cpp

--- tavşanı bulan kod.cpp
+++ tavşanı bulan kod.cpp
@@ -2,69 +2,78 @@
 
 using std::cout;
 using std::cin;
-int  n=0,tavsan,tahmin=2,deneme=0,ilerigeri=0,maxdeneme,i=0,t=-1,z=0;
+int sicrama();
+int yakalama();
+
+int tavsan, tahmin=2, yon=0 ,ilerigeri,kontrol=0,kontrol2=0,deneme=0;
+
 int main(){
-    cout<<"100 bosluklu tavsan bulma oyunu icin tavsanin deligini sen belirle oynuyacagini sec: ";
+    cout<<"100 bosluklu tavsan bulma oyunu icin tavsanin bulunacagi deligi sec:";
     cin>>tavsan;
-    cout<<"100 bosluklu tavsan bulma oyunu \n";
-    srand(time(NULL));
+    cout<<"\nTavsan bulma oyunu: \n";
 
     do
     {
-
-        ilerigeri = (rand() % 2) ;
-        if (ilerigeri == 1)
-        {
-            ilerigeri= -1;
-        }
-        else if (ilerigeri == 0)
-        {
-            ilerigeri = 1;
-        }
-        
-        
-        tavsan = tavsan + ilerigeri;
-
-
-        if (tavsan>=101)
-        {
-            tavsan = 99;
-        }
-        else if (tavsan<=0)
-        {
-            tavsan = 2;
-        }
-        cout<<"Tavsan bu delikte: "<<tavsan<<"\n";
-
-        if (tahmin==2)
-        {
-            i=1;
-            t=t+1;
-        }
-        else if (tahmin == 99 )
-        {
-            i=2;
-            z=z+1;
-        }
-        if(i==1 && t==2){
-        tahmin = tahmin+1;
-        }
-
-        else if (i==2 && z==2)
-        {
-            tahmin=tahmin-1;
-        }
-        cout<<"Tahminin bu: "<<tahmin<<"\n";
-        deneme=deneme+1;
-              
-
-    } while (tavsan != tahmin);
-
-        cout<<"Tavsan "<<tavsan<<" numarali deliginde iken tahminin: "<<tahmin<<"\n";
-        cout<<deneme<<". denemde buldun.\n";
-        
+        sicrama();
+        yakalama();
+        deneme = deneme+1;
+    } while (tahmin != tavsan);
+    
+    cout<<"Tavsan "<<tavsan<<" deliginde iken tahminin: "<<tahmin;
+    cout<<"\n"<<deneme<<". denemede yakaladin";
+    
     return 0;
 }
 
+
+int sicrama(){
+
+        ilerigeri = (rand()%2);
+        if (ilerigeri == 0)
+        {
+            ilerigeri=-1;
+        }
+        tavsan = tavsan + ilerigeri;
+
+        if (tavsan >= 101)
+        {
+            tavsan=99;
+        }
+        else if (tavsan <= 0)
+        {
+            tavsan = 2;
+        }
+        
+        cout<<"Tavsan bu delikte: "<<tavsan<<"\n";
+    return 0;
+}
+
+
+int yakalama() {
+
+    if (tahmin == 99)
+    {
+        yon = 1;
+        kontrol2 = kontrol2 + 1;
+    }
+    if (tahmin == 2)
+    {
+        yon = 0;
+        kontrol = kontrol + 1;
+    }
+
+    
+
+    if (yon == 0 && kontrol == 2)
+    {
+        tahmin = tahmin + 1;
+    }
+    else if (yon==1 && kontrol2 == 2)
+    {
+        tahmin = tahmin - 1;
+    }
+    cout<<"Tahminin bu :"<<tahmin<<"\n";
+    return 0;
+    }
 
 
